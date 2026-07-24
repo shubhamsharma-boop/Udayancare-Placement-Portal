93cfa6d24f2de20d44b0cc0a3763347d5df8b37f
shubhamsharma-boop <shubham.sharma@udayancare.org>
2026-07-24 13:34:25 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,27 @@
+// =================================
+// UCPP CONFIGURATION
+// Version : 1.0.0
+// =================================
+
+const CONFIG = {
+
+    APP_NAME: "UCPP",
+
+    APP_VERSION: "1.0.0",
+
+    API_URL: "https://script.google.com/macros/s/AKfycbzZok-P42uLsnpwr2ENRACeUVL7_xRZWu-meomwR64zyYuytLHunLsDaLSx2CVvd5dI/exec",
+
+    STORAGE: {
+
+        CANDIDATE: "ucpp_candidate",
+
+        EMPLOYER: "ucpp_employer",
+
+        ADMIN: "ucpp_admin"
+
+    }
+
+};
+
+// Freeze Configuration
+Object.freeze(CONFIG);
